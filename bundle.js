dac7dcb4676b508066df81a5a89bf9b9931fe7b7
Ken Chau <kchau@microsoft.com>
2021-12-02 13:23:16 -0800

    initial

--- bundle.js
+++ bundle.js
@@ -6,7 +6,6 @@ const path = require("path");
 /**
  *
  * @param {string} content
- * @returns
  */
 function parseForImport(content) {
   const lines = content.split(/\r?\n/);
@@ -27,26 +26,5 @@ function parseForImport(content) {
  */
 module.exports = function bundle(entry, src) {
   // TODO: write a iterative graph traversal "bundler" here
-
-  let result = [];
-  let visited = new Set();
-  let stack = [entry];
-
-  while (stack.length > 0) {
-    const mod = stack.pop();
-
-    if (!visited.has(mod)) {
-      visited.add(mod);
-      const content = fs.readFileSync(path.join(src, mod), "utf-8");
-      const { imports, parsed } = parseForImport(content);
-
-      result.unshift(parsed);
-
-      for (const imported of imports) {
-        stack.push(imported);
-      }
-    }
-  }
-
-  return result.join('\n');
+  return "the result";
 }
